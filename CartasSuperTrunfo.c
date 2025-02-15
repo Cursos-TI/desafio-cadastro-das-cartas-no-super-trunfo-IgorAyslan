a9d29072d44847ec68064cb1fae4dd516f25bb0e
IgorAyslan <150556413+IgorAyslan@users.noreply.github.com>
2025-02-15 21:25:27 +0000

    Corrigido erros na iompressão ao usuario

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,8 +8,8 @@
 
 int main() {
     char CodigodaCidade;
-    char NomedaCidade [30];
-    int população;
+    char nomedacidade [30];
+    int populacao;
     double area;
     float PIB;
     int PontosTuristicos;
@@ -19,37 +19,28 @@ int main() {
     scanf("%c", &CodigodaCidade);
 
     printf("Nome da cidade\n");
-    scanf("%s",&NomedaCidade);
+    scanf("%s",&nomedacidade);
 
     printf("Qual é a população?\n");
-    scanf("%d", &população);
+    scanf("%d", &populacao);
 
-    printf("Qual o tamanho da área?\p");
-    scanf("%e", &area);
+    printf("Qual o tamanho da área?\n");
+    scanf("%d", &area);
 
     printf("Qual o PIB desta cidade?\n");
-    scanf("%3.3f", &PIB);
+    scanf("%f", &PIB);
 
     printf("Possui quantos pontos turísticos?\n");
     scanf("%d", &PontosTuristicos);
 
     printf("Esta é a sua cidade!\n");
-    printf("Nome da cidade: %s - Código: %c\n", NomedaCidade, CodigodaCidade);
-    printf("População: %d\n", população);
-    printf("Área: %e\n", area);
-    printf("PIB: %3.3f\n", PIB);
+    printf("Nome da cidade: %s\n", nomedacidade);
+    printf("Codigo da cidade: %c\n", CodigodaCidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %d\n", area);
+    printf("PIB: %.2f\n", PIB);
     printf("Pontos turísticos: %d\n", PontosTuristicos);
 
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
 }
